4e0ac4275dcae6be180fd467dcee41b6a03e1b53
PalMassimo <massimo.palmisano02@gmail.com>
2021-08-24 18:25:16 +0200

    Try to make unversioned files versioned

---
+++ app/src/main/java/it/units/musicplatform/activities/LoginActivity.kt
@@ -0,0 +1,12 @@
+package it.units.musicplatform.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import it.units.musicplatform.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
