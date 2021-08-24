7ac84d9727fbcf88b307dfa447b4183960279cbb
PalMassimo <massimo.palmisano02@gmail.com>
2021-08-24 22:08:42 +0200

    Added bottom navigation bar menu

--- app/src/main/java/it/units/musicplatform/activities/LoginActivity.kt
+++ app/src/main/java/it/units/musicplatform/activities/LoginActivity.kt
@@ -1,12 +1,46 @@
 package it.units.musicplatform.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import it.units.musicplatform.R
+import it.units.musicplatform.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        auth = FirebaseAuth.getInstance()
+
+//        val emailEditText = findViewById<EditText>(R.id.emailEditText)
+//        val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
+//        val button: Button = findViewById(R.id.loginButton)
+//
+//        val email: String = emailEditText.text.toString()
+//        val password: String = passwordEditText.text.toString()
+
+        binding.loginButton.setOnClickListener {
+            auth.signInWithEmailAndPassword(binding.emailEditText.text.toString().trim(), binding.passwordEditText.text.toString().trim()).addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this, MainActivity::class.java).apply { putExtra(getString(R.string.user_id), auth.currentUser?.uid) }
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "Login failed, check credentials", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+
     }
+
+
 }
--- app/src/main/java/it/units/musicplatform/activities/MainActivity.kt
+++ app/src/main/java/it/units/musicplatform/activities/MainActivity.kt
@@ -2,11 +2,35 @@ package it.units.musicplatform.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import it.units.musicplatform.R
+import it.units.musicplatform.databinding.ActivityLoginBinding
+import it.units.musicplatform.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var userId = intent.getStringExtra(getString(R.string.user_id))
+
+
+    }
+
+    fun homeNavigationBarListener(item: MenuItem) {
+        item.setCheckable(true)
+    }
+
+    fun searchNavigationBarListener(item: MenuItem) {
+        item.setCheckable(true)
+    }
+
+    fun profileNavigationBarListener(item: MenuItem) {
+        item.setCheckable(true)
     }
 }
