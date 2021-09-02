aabea0c00bece54ed119bfbd59f9225cf46362b0
PalMassimo <massimo.palmisano02@gmail.com>
2021-09-02 15:10:01 +0200

    Now user can change his/her profile image

--- app/src/main/java/it/units/musicplatform/fragments/ProfileFragment.kt
+++ app/src/main/java/it/units/musicplatform/fragments/ProfileFragment.kt
@@ -5,16 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResultListener
 import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 import it.units.musicplatform.adapters.UserPostsAdapter
 import it.units.musicplatform.databinding.FragmentProfileBinding
 import it.units.musicplatform.retrievers.StorageReferenceRetriever
 import it.units.musicplatform.utilities.GlideApp
 import it.units.musicplatform.viewmodels.UserViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
@@ -41,7 +46,14 @@ class ProfileFragment : Fragment() {
         userViewModel = ViewModelProviders.of(requireActivity()).get(UserViewModel::class.java)
         binding.userviewmodel = userViewModel
 
-        GlideApp.with(requireContext()).load(StorageReferenceRetriever.userImageReference(userId)).into(binding.profileImageView)
+        val newProfileImageLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            Glide.with(requireContext()).load(uri).into(binding.profileImageView)
+            StorageReferenceRetriever.userImageReference(userId).putFile(uri)
+            requireActivity().lifecycleScope.launch(Dispatchers.Default) { GlideApp.get(requireContext()).clearDiskCache() }
+        }
+
+        binding.profileImageView.setOnClickListener { newProfileImageLauncher.launch("image/*") }
+        GlideApp.with(requireContext()).load(StorageReferenceRetriever.userImageReference(userId)).skipMemoryCache(true).into(binding.profileImageView)
 
         setUpRecyclerView()
 
